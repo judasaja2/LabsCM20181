9a899e390553be292a5f4622e4d3fb7d15485522
killerwp <johanc.suarez@hotmail.com>
2018-03-01 15:16:05 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/udea/compumovil/gr05_20181/labscm20181/MainActivity.java
@@ -0,0 +1,13 @@
+package co.edu.udea.compumovil.gr05_20181.labscm20181;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
